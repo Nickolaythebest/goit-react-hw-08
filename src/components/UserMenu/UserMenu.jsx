f8e68ac0976b9228a93fbe0cc8b6be9be0382101
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-02-09 15:46:34 +0100

    LogOut

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,12 +1,14 @@
 import { Button, Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import { selectUser } from "../../redux/auth/selectors";
+
 import { useDispatch, useSelector } from 'react-redux';
-import { logoutThunk } from "../../redux/auth/operations";
+import { logoutThunk } from "../../redux/contacts/operations";
+import { selectUser } from "../../redux/auth/selectors";
 
 
 function UserMenu() {
     const user = useSelector(selectUser);
+    
     const dispatch = useDispatch();
     return (
         <Box sx={{ 
